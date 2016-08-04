0fe1cfb83260f9f4395947787f433440fff61940
giota-cliqz <panagiota@cliqz.com>
2016-08-04 16:46:23 +0200

    Form validation and login form styles by joseph

--- bookshelf.py
+++ bookshelf.py
@@ -6,6 +6,8 @@ import sqlite3
 from flask import Flask, request, session, g, redirect, url_for, abort, \
 	  render_template, flash, send_from_directory
 from werkzeug.utils import secure_filename
+from wtforms import Form, BooleanField, StringField, PasswordField, validators
+
 
 UPLOAD_FOLDER = 'uploads'
 
@@ -21,6 +23,15 @@ app.config.update(dict(
 	PASSWORD='default'
 ))
 app.config.from_envvar('BOOKSHELF_SETTINGS', silent=True)
+
+class LoginForm(Form):
+	email = StringField('Email', [validators.input_required()])
+	password = PasswordField('Password', [validators.DataRequired()])
+class RegistrationForm(Form):
+	first_name = StringField('Firstname', [validators.Length(min=4, max=25)])
+	last_name = StringField('Lastname', [validators.Length(min=4, max=25)])
+	email = StringField('Email Address', [validators.Length(min=6, max=35)])		
+	password = PasswordField('Password', [validators.DataRequired()])					
 
 def connect_db():
 	"""Connects to the database"""
@@ -95,32 +106,36 @@ def borrowed():
 	borrowed = cur.fetchall()
 	return render_template('show_borrowed.html', borrowed=borrowed)
 
-@app.route('/login', methods=['GET'])
-def show_login():
-	return render_template('login.html')
 
-@app.route('/login', methods=['POST'])
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    error = None
-    db = get_db()
-    result = db.execute('select * from users where email=? and password=?', [request.form['email'], request.form['password']])
-    cursor = result.fetchone()
-    if cursor is None:
-    	print ('Invalid email or password', file=sys.stderr)
-    	error = 'Invalid email or password'
-    	return render_template('login.html', error=error)
+	form = LoginForm(request.form)
+	if request.method =='POST':
+	    error = None
+	    print(form)
+	    db = get_db()
+	    result = db.execute('select * from users where email=? and password=?', [form.email.data, form.password.data])
+	    cursor = result.fetchone()
 
-    print ('OK.', file=sys.stderr)
-    session['logged_in'] = True 
-    session['isAdmin'] = cursor['admin']
-    session['email'] = cursor['email']
-    session['user_id'] = cursor['id']
-    return redirect(url_for('show_books'))
+	    if cursor is None or not form.validate():
+	    	error = 'Invalid email or password'
+	    	return render_template('login.html', error=error, form=form)
+
+	    print ('OK.', file=sys.stderr)
+	    session['logged_in'] = True 
+	    session['isAdmin'] = cursor['admin']
+	    session['email'] = cursor['email']
+	    session['user_id'] = cursor['id']
+	    return redirect(url_for('show_books'))
+	return render_template('login.html', form=form)    
     # 
 
 @app.route('/logout')
 def logout():
     session.pop('logged_in', None)
+    session.pop('isAdmin', None)
+    session.pop('email', None)
+    session.pop('user_id', None)
     flash('You were logged out')
     return redirect(url_for('show_books'))
 
@@ -133,19 +148,25 @@ def show_users():
 	users = cur.fetchall()
 	return render_template('show_users.html', users=users)
 
-@app.route('/users/add', methods=['GET'])
-def show_add_user():
-	return render_template('add_user.html')
+# @app.route('/users/add', methods=[])
+# def show_add_user():
+	
 
-@app.route('/users/add', methods=['POST'])
+@app.route('/users/add', methods=['GET', 'POST'])
 def add_user():
-	if not session.get('logged_in'):
-		abort(401)
-	db = get_db()
-	db.execute('insert into users (first_name, last_name, email, password, admin) values(?, ?, ?, ?, 0)', [request.form['first_name'], request.form['last_name'], request.form['email'], request.form['password']])
-	db.commit()
-	flash('New user added Successfully')
-	return redirect(url_for('show_users'))
+	form = RegistrationForm(request.form)
+	if request.method == 'POST' and form.validate():
+		print ('OK1.', file=sys.stderr) 
+		if not session.get('logged_in'):
+			abort(401)
+		db = get_db()
+		db.execute('insert into users (first_name, last_name, email, password, admin) values(?, ?, ?, ?, 0)', [form.first_name.data, form.last_name.data, form.email.data, form.password.data])
+		db.commit()
+		flash('New user added Successfully')
+		print ('OK2.', file=sys.stderr)
+		return redirect(url_for('show_users'))
+	print ('OK3.', file=sys.stderr)	
+	return render_template('add_user.html', form=form)
 
 if __name__ == '__main__':
 	if len(sys.argv)>1 and sys.argv [1]== 'initdb':
